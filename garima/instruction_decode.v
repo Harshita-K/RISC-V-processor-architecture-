f7063c3df9d9706fe36384a3d7a5b3f64c445f4e
harshita <harshita.kumari@students.iiit.ac.in>
2025-03-08 21:46:07 +0530

    u

--- garima/instruction_decode.v
+++ garima/instruction_decode.v
@@ -5,7 +5,6 @@ module instruction_decode(
     output [4:0] write_addr,
     output [9:0] alu_control, // Updated to 10-bit concatenation
     output [1:0] ALUOp,
-    output reg[31:0] imm_val,
     output ALUSrc,
     output RegWrite,
     output MemRead,
@@ -36,24 +35,6 @@ module instruction_decode(
         .ALUOp(ALUOp),
         .invOp(invOp)
     );
-
-    // Immediate extraction logic
-    always @(*) begin
-        case (opcode)
-            7'b0000011, 7'b0010011:  // I-type (Load/Immediate arithmetic)
-                imm_val = {{20{instruction[31]}}, instruction[31:20]};
-            7'b0100011:  // S-type (Store)
-                imm_val = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
-            7'b1100011:  // B-type (Branch)
-                imm_val = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
-            7'b1101111:  // J-type (JAL)
-                imm_val = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
-            7'b0110111, 7'b0010111:  // U-type (LUI/AUIPC)
-                imm_val = {instruction[31:12], 12'b0};
-            default: 
-                imm_val = 32'bx; // Invalid case
-        endcase
-    end
 
 endmodule
 
@@ -193,4 +174,3 @@ module Mux(
 );
     assign out = select ? input2 : input1;
 endmodule
-
