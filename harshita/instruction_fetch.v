ea47c858577ad07254231680f7ff3d87f3f87af3
harshita <harshita.kumari@students.iiit.ac.in>
2025-03-11 19:32:05 +0530

    h

--- harshita/instruction_fetch.v
+++
@@ -1,8 +0,0 @@
-`include "global.v"
-
-module instruction_fetch(
-    input [63:0] PC,
-    output [31:0] instruction
-);
-    assign instruction = instr_mem[PC[11:2]]; // Fetch instruction based on PC value
-endmodule
--- harshita/write_back.v
+++
@@ -1,9 +0,0 @@
-`include "global.v"
-module write_back(
-    input [63:0] alu_result,
-    input [63:0] mem_data,
-    input MemtoReg,
-    output [63:0] write_data
-);
-    assign write_data = MemtoReg ? mem_data : alu_result;
-endmodule
